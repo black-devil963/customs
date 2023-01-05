eb95ecad7b003c02d1f81577a615eb5bbf568e33
black-devil963 <89310171+black-devil963@users.noreply.github.com>
2023-01-05 23:16:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const options = {
   request.post(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
      
-     res.send({body.id});
+     res.send(body);
      
     }
     else res.send(error+response.statusCode);
