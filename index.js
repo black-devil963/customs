4f64c08a4995124e185df1c33ca0546330f77480
black-devil963 <89310171+black-devil963@users.noreply.github.com>
2023-01-05 23:00:57 +0530

    tata

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const options = {
 }
   request.post(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-    res.sendfile("https://github.com/black-devil963/customs/blob/main/index.html");
+    res.send(body);
     }
     else res.send(error+response.statusCode);
   })
