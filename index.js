03639671ceedd034b846702f11c7ee3657e30969
nikhil kumar aman <nikhilkumaraman963@gmail.com>
2023-01-05 20:35:16 +0530

    s

---
+++ index.js
@@ -0,0 +1,31 @@
+var express=require("express");
+var cors=require("cors");
+const Razorpay = require('razorpay');
+var app=express();
+app.use(cors());
+const request = require('request');
+var credentials = btoa("rzp_test_0a359K3M2JK1sI:40C2WbRdKCEy7sKLSqbKR3BE");
+const options = {
+  url: 'https://api.razorpay.com/v1/orders',
+  json: true,
+  headers: {
+      "Authorization": `Basic ${credentials}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json;'
+    },
+  body: {
+    "amount": 32550,
+      "currency": "INR",
+      "receipt": "receipt#1"
+  }
+}
+ app.get("/",function (req,res){
+  request.post(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body); // Print the google web page.
+    }
+    else res.send(error+response.statusCode);
+  })
+});
+app.listen(5050,() => console.log("Server is running"));
+
